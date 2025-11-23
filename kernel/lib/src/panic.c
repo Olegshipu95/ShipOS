173958007bcb8f1931ab177163c50319facda52f
Armemius <rusgy@yandex.ru>
2025-11-23 19:31:45 +0300

    feat: add writes to serial port on panic

--- kernel/lib/src/panic.c
+++ kernel/lib/src/panic.c
@@ -1,7 +1,13 @@
 #include "../include/panic.h"
+#include "../../serial/serial.h"
 
 void panic(char *message) {
+    // Output to both VGA and serial for maximum visibility
     print(message);
     print("\tpanic!");
+    
+    serial_printf("[PANIC] %s\n", message);
+    serial_printf("[PANIC] System halted!\n");
+    
     while (1) {}
 }
