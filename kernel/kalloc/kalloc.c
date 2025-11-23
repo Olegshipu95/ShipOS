0a2fc1293929a9ea79088107ae584505791cfbc7
Vainslav <Vladik_ww@mail.ru>
2025-11-23 13:34:28 +0300

    Add slab

--- kernel/kalloc/kalloc.c
+++ kernel/kalloc/kalloc.c
@@ -22,6 +22,8 @@ void kinit(uint64_t start, uint64_t stop) {
     p = (char *) PGROUNDUP(start);
     for (; p + PGSIZE < stop; p += PGSIZE)
         kfree(p);
+
+    init_slab_cache();
 }
 
 void kfree(void *pa) {
