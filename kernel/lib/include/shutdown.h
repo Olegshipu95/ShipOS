1d1214a94a2051f3860d4b0f1e91beb3486cb008
Armemius <rusgy@yandex.ru>
2025-11-28 19:12:00 +0300

    feat: add shutdown functionality

---
+++ kernel/lib/include/shutdown.h
@@ -0,0 +1,25 @@
+//
+// Created by ShipOS developers on 28.11.25.
+// Copyright (c) 2025 SHIPOS. All rights reserved.
+//
+// System shutdown interface for ShipOS kernel.
+// Provides graceful shutdown via ACPI or port I/O methods.
+//
+
+#ifndef SHIPOS_SHUTDOWN_H
+#define SHIPOS_SHUTDOWN_H
+
+/**
+ * @brief Gracefully shut down the operating system
+ * 
+ * Attempts to power off the system using multiple methods:
+ * 1. QEMU/Bochs shutdown port (0x604)
+ * 2. Legacy APM interface
+ * 3. ACPI sleep state (if available)
+ * 
+ * If hardware shutdown is not supported, the system will halt.
+ * This function does not return.
+ */
+void shutdown(void);
+
+#endif // SHIPOS_SHUTDOWN_H
---
+++ kernel/lib/src/shutdown.c
@@ -0,0 +1,64 @@
+//
+// Created by ShipOS developers on 28.11.25.
+// Copyright (c) 2025 SHIPOS. All rights reserved.
+//
+// System shutdown implementation for ShipOS kernel.
+//
+
+#include "../include/shutdown.h"
+#include "../include/logging.h"
+#include "../include/x86_64.h"
+
+/**
+ * @brief QEMU debug-exit device port
+ * Using isa-debug-exit device at port 0xf4
+ * Exit code = (value << 1) | 1, so 0x00 gives exit code 1
+ */
+#define QEMU_DEBUG_EXIT_PORT 0xf4
+#define QEMU_EXIT_SUCCESS 0x10  // Will result in QEMU exit code 33 ((0x10 << 1) | 1)
+
+/**
+ * @brief ACPI shutdown port for QEMU/Bochs
+ * Writing 0x2000 to port 0x604 triggers a shutdown in QEMU
+ */
+#define QEMU_SHUTDOWN_PORT 0x604
+#define QEMU_SHUTDOWN_VALUE 0x2000
+
+/**
+ * @brief Legacy APM (Advanced Power Management) interface
+ * Used by older systems for power management
+ */
+#define APM_PORT 0xB004
+#define APM_SHUTDOWN_VALUE 0x2000
+
+void shutdown(void) {
+    LOG("System shutdown initiated");
+    LOG_SERIAL("SHUTDOWN", "Shutting down ShipOS...");
+
+    // Method 1: QEMU debug-exit device (most reliable for automated testing)
+    LOG_SERIAL("SHUTDOWN", "Attempting QEMU debug-exit (port 0xf4)");
+    outl(QEMU_DEBUG_EXIT_PORT, QEMU_EXIT_SUCCESS);
+
+    // Method 2: QEMU/Bochs shutdown via port 0x604
+    LOG_SERIAL("SHUTDOWN", "Attempting QEMU/Bochs shutdown (port 0x604)");
+    outw(QEMU_SHUTDOWN_PORT, QEMU_SHUTDOWN_VALUE);
+
+    // Legacy APM shutdown
+    LOG_SERIAL("SHUTDOWN", "Attempting APM shutdown (port 0xB004)");
+    outw(APM_PORT, APM_SHUTDOWN_VALUE);
+
+    // Try older QEMU shutdown port
+    LOG_SERIAL("SHUTDOWN", "Attempting legacy QEMU shutdown (port 0x501)");
+    outb(0x501, 0x01);
+
+    // Hardware shutdown failed
+    LOG_SERIAL("SHUTDOWN", "Hardware shutdown not supported");
+    LOG("Hardware shutdown not supported - system halted");
+    print("System shutdown requested but hardware shutdown is not available.\n");
+    print("It is now safe to power off your computer.\n");
+
+    // Halt the CPU indefinitely
+    while (1) {
+        asm volatile("hlt");
+    }
+}
