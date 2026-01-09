9e8f998cdedec37b0404f999addd76ad39b2f301
Armemius <rusgy@yandex.ru>
2026-01-09 17:29:17 +0300

    feat: add memcmp function

---
+++ kernel/lib/include/memcmp.h
@@ -0,0 +1,14 @@
+//
+// Created by ShipOS developers on 09.01.26.
+// Copyright (c) 2026 SHIPOS. All rights reserved.
+//
+
+#ifndef SHIP_OS_MEMCMP_H
+#define SHIP_OS_MEMCMP_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+int memcmp(const void *a, const void *b, size_t n);
+
+#endif
